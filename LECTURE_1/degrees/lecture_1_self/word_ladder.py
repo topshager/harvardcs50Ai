c463083c78b8db28d72c5a0ed321ea5397d8c9ca
topshager <rem28777@gmail.com>
2025-05-02 00:16:01 +0200

    Create word_ladder.py

---
+++ LECTURE_1/degrees/lecture_1_self/word_ladder.py
@@ -0,0 +1,31 @@
+from collections import deque
+start = "hit"
+goal = "cog"
+dictionary = {"hot", "dot", "dog", "lot", "log", "cog"}
+
+
+
+def word_ladder(start,goal,dictionary ):
+
+    if goal not in dictionary:
+        return None
+    
+    que = deque()
+    que.append((start,[start])) 
+
+    while que:
+
+        current_word,path = que.popleft()
+
+        if current_word == goal:
+            return path
+        
+        for i in range(len(current_word)):
+            for c in  'abcdefghijklmnopqrstuvwxyz':
+                next_word =current_word[:1] + c +current_word[i+1]
+                if next_word in dictionary and next_word not in path:
+                    que.append((next_word,path+[next_word]))
+    return None
+
+
+    
