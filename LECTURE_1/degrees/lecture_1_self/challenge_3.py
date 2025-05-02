35772f99b04e882d46e293b414d6a2daf3e34951
topshager <rem28777@gmail.com>
2025-05-02 23:11:57 +0200

    Update challenge_3.py

---
+++ LECTURE_1/degrees/lecture_1_self/challenge_3.py
@@ -0,0 +1,11 @@
+"""
+✅ 2. Maze Solver with DFS & BFS (You Choose Start and Goal)
+Write a program that takes any maze grid (as a 2D list) and lets the user specify a start and goal. Implement both:
+
+DFS: show any path (not necessarily shortest)
+
+BFS: show the shortest path
+
+Try with walls (#), free spaces ( ), start (A), goal (B)
+
+"""
