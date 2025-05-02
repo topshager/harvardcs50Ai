60f3523c11f5e020001a959ed5d52cf51c1ebbd7
topshager <rem28777@gmail.com>
2025-05-02 23:12:03 +0200

    Update challenge_2.py

--- LECTURE_1/degrees/lecture_1_self/challenge_2.py
+++ LECTURE_1/degrees/lecture_1_self/challenge_2.py
@@ -36,7 +36,7 @@ def DFS(start,goal,maze):
             nx,ny = x + dx,y + dy
             if 0 <=nx <len(maze) and 0 <= ny <len(maze[0]):
                 if maze[nx][ny] != " " and (nx,ny)  not in visited:
-                    stack.append((nx,ny),path +[(nx+ ny)]) 
+                    stack.append(((nx,ny),path +[(nx, ny)]))
     return  None
 
 
@@ -47,16 +47,18 @@ def BFS(start ,goal,maze):
     
     if not maze:
         return None
+        
     queque.append((start,[start]))
+    
     while queque:
-        (x,y),path = queque.pop(-1)
+        (x,y),path = queque.popleft()
 
         if (x,y) in visited:
             continue
 
         visited.add((x,y))
 
-        if maze[i][y] == "B":
+        if maze[x][y] == "B":
             return path 
         
 
@@ -64,12 +66,10 @@ def BFS(start ,goal,maze):
             nx,ny  = x + dx,y +dy
             if 0 <= nx <len(maze) and 0 <= ny <len(maze[0]):
                 if maze[x][y] != " " and (nx,ny) not in visited:
-                    queque.append((nx,ny),path + [(nx + ny)])
+                    queque.append(((nx,ny),path + [(nx , ny)]))
 
     return None
 
         
-
-
-    
-
+print("DFS Path:", DFS(start, goal, maze))
+print("BFS Path:", BFS(start, goal, maze))
