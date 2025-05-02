ff16005da11c1f233ce565977c8191fb963eabb9
topshager <rem28777@gmail.com>
2025-05-02 21:23:30 +0200

    Update challenge_2.py

---
+++ LECTURE_1/degrees/lecture_1_self/challenge_2.py
@@ -0,0 +1,44 @@
+from collections import deque
+maze =[
+["A","0"," "," "," "],
+[" ","0","0","0","0"],
+[" ","0"," "," ","0"],
+[" ","0"," "," ","B"]
+]
+
+
+start = (0,0)
+goal = (3,4)
+
+
+def DFS(start,goal,maze):
+
+    stack = deque()
+    visited = set()
+
+    if not maze:
+        return None
+    
+    stack.append((start,[start]))
+    while stack:
+        (x,y),path = stack.pop()
+
+        if (x,y) in visited:
+            continue
+
+        visited.add((x,y))
+
+        if maze[x][y] == "B":
+            return path
+        
+
+        for dx,dy in[(-1,0),(1,0),(0,-1),(0,1)]:
+            nx,ny = x + dx,y + dy
+            if 0 <=nx <len(maze) and 0 <= ny <len(maze[0]):
+                if maze[nx][ny] != " " and (nx,ny)  not in visited:
+                    stack.append((nx,ny),path +(nx+ ny)) 
+    return  None
+
+
+
+
