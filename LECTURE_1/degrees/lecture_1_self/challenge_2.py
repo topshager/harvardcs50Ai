1250f8240297b95ac55576917e13a3ccd513f9fa
topshager <rem28777@gmail.com>
2025-05-02 22:23:59 +0200

    misc

--- LECTURE_1/degrees/lecture_1_self/challenge_2.py
+++ LECTURE_1/degrees/lecture_1_self/challenge_2.py
@@ -36,9 +36,40 @@ def DFS(start,goal,maze):
             nx,ny = x + dx,y + dy
             if 0 <=nx <len(maze) and 0 <= ny <len(maze[0]):
                 if maze[nx][ny] != " " and (nx,ny)  not in visited:
-                    stack.append((nx,ny),path +(nx+ ny)) 
+                    stack.append((nx,ny),path +[(nx+ ny)]) 
     return  None
 
 
+def BFS(start ,goal,maze):
 
+    queque = deque()
+    visited = set()
+    
+    if not maze:
+        return None
+    queque.append((start,[start]))
+    while queque:
+        (x,y),path = queque.pop(-1)
+
+        if (x,y) in visited:
+            continue
+
+        visited.add((x,y))
+
+        if maze[i][y] == "B":
+            return path 
+        
+
+        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+            nx,ny  = x + dx,y +dy
+            if 0 <= nx <len(maze) and 0 <= ny <len(maze[0]):
+                if maze[x][y] != " " and (nx,ny) not in visited:
+                    queque.append((nx,ny),path + [(nx + ny)])
+
+    return None
+
+        
+
+
+    
 
