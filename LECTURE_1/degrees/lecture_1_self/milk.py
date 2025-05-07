f7d77ad71739775ce12b6f27762b8bb42b17cb61
topshager <rem28777@gmail.com>
2025-05-07 13:16:40 +0200

    Create milk.py

---
+++ LECTURE_1/degrees/lecture_1_self/milk.py
@@ -0,0 +1,18 @@
+"""
+✅ 6. Water Jug Problem (State Space BFS)
+You have:
+
+A 4-liter jug
+
+A 3-liter jug
+
+You must measure exactly 2 liters using these operations:
+
+Fill, Empty, Pour
+
+Return a sequence of states showing how to get 2 liters in one jug.
+
+📌 This is state-space search, like AI search problems.
+"""
+
+j
