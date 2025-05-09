7cc4d940943c11e63e2376f15efa24ea65038c0a
topshager <rem28777@gmail.com>
2025-05-09 20:19:53 +0200

    Create cannibal_and_missionary_problem.py

---
+++ LECTURE_1/degrees/lecture_1_self/cannibal_and_missionary_problem.py
@@ -0,0 +1,6 @@
+"""
+🔢 1. Missionaries and Cannibals Problem
+Goal: Move 3 missionaries and 3 cannibals across a river using a boat that can carry 2 people, without ever having cannibals outnumber missionaries on either side.
+
+
+"""
