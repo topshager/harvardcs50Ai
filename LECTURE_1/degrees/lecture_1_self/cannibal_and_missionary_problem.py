c94e18f430a73c288870bc5f4bb899a691d90da1
topshager <rem28777@gmail.com>
2025-05-10 22:13:21 +0200

    Update cannibal_and_missionary_problem.py

--- LECTURE_1/degrees/lecture_1_self/cannibal_and_missionary_problem.py
+++ LECTURE_1/degrees/lecture_1_self/cannibal_and_missionary_problem.py
@@ -1,6 +1,14 @@
+from collections import deque
 """
 🔢 1. Missionaries and Cannibals Problem
-Goal: Move 3 missionaries and 3 cannibals across a river using a boat that can carry 2 people, without ever having cannibals outnumber missionaries on either side.
-
-
+Goal: Move 3 missionaries and 3 cannibals across a river using a boat that can carry 2 people, without ever having cannibals outnumber missionaries on either side.§§
 """
+
+M_left = 0
+C_left = 0
+boat_side = {"l","r"}
+
+
+def crossing(m,c,side):
+
+    
