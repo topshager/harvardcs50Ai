7a33d90676d6c64e4ff0b0911e729a7fcf2345d3
topshager <rem28777@gmail.com>
2025-05-24 23:10:20 +0200

    alg

---
+++ LECTURE_1/lecture_1/sliding_puzzle.py
@@ -0,0 +1,10 @@
+"""
+1. 🔀 Sliding Puzzle Solver (8-puzzle)
+Implement an AI agent that, given any initial configuration of the 8-puzzle, returns the shortest sequence of moves to reach the goal configuration using A* search with admissible heuristics.
+
+"""
+
+puzzle = ([],
+          [],
+          [])
+
